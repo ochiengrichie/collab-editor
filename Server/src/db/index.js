22bce12332cfaa112a49d48cc9ffa0b040a5567d
Richard Onyango <ochiengrichie24@gmail.com>
2026-04-07 08:33:37 +0300

    Refactor database to have one url connection

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -1,12 +1,11 @@
-import pkg from 'pg';
+import pkg from "pg";
 const { Pool } = pkg;
 
 const db = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default db;
